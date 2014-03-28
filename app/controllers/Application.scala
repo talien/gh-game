ad923366210bcbb3f348aee1876b33e2a348fa9b
Tusa Viktor <tusavik@gmail.com>
2014-03-28 02:59:26 +0100

    Added imports for securesocial

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,8 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import securesocial.core.{IdentityId, UserService, Identity, Authorization}
+import securesocial.controllers.TemplatesPlugin
 
 object Application extends Controller with securesocial.core.SecureSocial {
   
