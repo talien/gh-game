d1e793a453171b99298980e440aa1b81a4a63ac7
Tusa Viktor <tusavik@gmail.com>
2014-04-01 23:35:18 +0200

    Added repo listing for login page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,13 +2,29 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import securesocial.core.{IdentityId, UserService, Identity, Authorization}
+import securesocial.core.{IdentityId, UserService, Identity, Authorization, OAuth2Info}
 import securesocial.controllers.TemplatesPlugin
+import play.api.libs.ws._
+import scala.concurrent.Future
+import scala.concurrent._
+import scala.concurrent.duration._
 
 object Application extends Controller with securesocial.core.SecureSocial {
   
   def index = SecuredAction { implicit request => {
-      Ok(views.html.index(request.user))
+      implicit val context = scala.concurrent.ExecutionContext.Implicits.global
+
+      val accessToken = request.user.oAuth2Info match {
+        case Some(info) => info.accessToken
+        case None => ""
+      }
+            
+      val futureResult: Future[Seq[String]] = WS.url("https://api.github.com/user/repos").withQueryString("access_token" -> accessToken).get().map {
+          response =>
+              (response.json \\ "name") map (_.as[String])
+      }
+      val repos = Await.result(futureResult, 30 seconds)
+      Ok(views.html.index(request.user, repos))
     }
   }
 }
