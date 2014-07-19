d8bcb8ca0e7edb76f0f0b141c676025ed5c21470
Tusa Viktor <tusavik@gmail.com>
2014-07-19 20:29:57 +0200

    Added hiccup, and use hiccup for controllers

--- src/gh_game/app.clj
+++ src/gh_game/app.clj
@@ -5,15 +5,36 @@
             [cemerick.friend :as friend]
             [clj-http.client :as client]
             [cheshire.core :as j]
-            [gh-game.github.auth :as gh-auth]))
+            [gh-game.github.auth :as gh-auth]
+            [hiccup.core :refer :all]
+            [hiccup.page :refer :all]
+            [hiccup.element :refer :all]
+            ))
 
 (declare render-status-page)
 (declare render-repos-page)
 
+(defn main-template [& body-content]
+    (html5 [:head 
+               [:title "Github Bounties"]
+               [:link {:href "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" :rel "stylesheet"}]
+           ]           
+         [:body body-content]
+    ))
+
+
+(defn index [request]
+   (main-template [:a.btn {:href "/repos" } "My Github Repositories"] 
+                  [:br] 
+                  [:a.btn {:href "/status"} "Status"] 
+                  [:br] 
+                  [:a.btn {:href "/logout"} "Logout"] 
+   ) )
+
 (defroutes gh-game-app
-  (GET "/" request "<a href=\"/repos\">My Github Repositories</a><br><a href=\"/status\">Status</a>")
+  (GET "/" request index)
   (GET "/status" request
-       (render-status-page request))
+       render-status-page)
   (GET "/repos" request (gh-auth/github-authorized render-repos-page))
 
   (friend/logout (ANY "/logout" request (ring.util.response/redirect "/"))))
@@ -41,5 +62,10 @@
 
 (defn render-repos-page
   [request]
-  (str (vec (map :name (get-github-repos (gh-auth/get-access-token-from-request request))))))
+  (main-template [:ul.list-group 
+   (for [alma (get-github-repos (gh-auth/get-access-token-from-request request))]
+    [:li.list-group-item
+      [:a.btn {:href (:html_url alma)} (:name alma)]
+    ]
+   )])) 
 
